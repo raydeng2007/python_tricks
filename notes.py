61128a8cb1370cbf9bdc005ea74247a56c32af3c
raydeng2007 <r6deng@uwaterloo.ca>
2019-02-14 11:06:54 -0500

    added notes

--- notes.py
+++ notes.py
@@ -1,2 +1,9 @@
 ########### TO BE ABLE TO SORT A DICT BY IT'S VALUE, RETURNS A LIST OF TUPLES OF (KEYS,VALUE)
 s = [(k, d[k]) for k in sorted(d, key=d.get, reverse=True)]
+
+########### TO POPULATE A DATAFRAME FROM A FOR LOOP, IT'S MUCH MORE EFFICIENT TO MAKE THE DATA
+########### INTO A LIST OF DICTS INSTEAD OF APPENDING A DATAFRAME TO A DATAFRAME AT EACH LOOP.
+lista = []
+for order in order_list:
+    lista.append(order.attributes)
+df = pd.DataFrame(lista)
